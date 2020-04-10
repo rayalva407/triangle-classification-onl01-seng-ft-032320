8770f03d5ce73fb0bb97e88658ac83b4f13b0b85
Ray Alva <rayalva407@gmail.com>
2020-04-10 14:24:49 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,13 @@
 class Triangle
+  attr_accessor :equilateral, :isosceles, :scalene
   
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
+  end
+  
+  def kind
+    
   end
 end
