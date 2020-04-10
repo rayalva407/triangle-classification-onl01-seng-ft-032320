4412d1c1f75d1d85cc57a6265253e8ec0213ed56
Ray Alva <rayalva407@gmail.com>
2020-04-10 15:23:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,5 @@
+require pry
+
 class Triangle
   
   def initialize(side1, side2, side3)
@@ -12,6 +14,7 @@ class Triangle
     elsif (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2) || (@side2 + @side3 <= @side1)
       true
     end
+    binding.pry
   end
     
   def kind
