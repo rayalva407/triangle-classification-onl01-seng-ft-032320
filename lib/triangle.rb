9b860f48f03ef2f32bb244218808b2effb464cff
Ray Alva <rayalva407@gmail.com>
2020-04-10 15:17:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,7 +22,7 @@ class Triangle
         :equilateral
       elsif (@side1 == @side2) || (@side2 == @side3) || (@side3 == @side1)
         :isosceles
-      elsif ()
+      elsif (@side1 != @side2) && (@side2 != @side3)
   end
   
   class TriangleError < StandardError
