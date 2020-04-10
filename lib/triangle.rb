a152fc5b7100d6932572814002b6c8ecd19005c3
Ray Alva <rayalva407@gmail.com>
2020-04-10 14:58:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Triangle
   
   def initialize(side1, side2, side3)
@@ -7,11 +9,11 @@ class Triangle
   end
   
   def invalid?
-    ((@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)) || 
+    ((@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)) || ((@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1))
   end
     
   def kind
-    
+    binding.pry
   end
   
   class TriangleError < StandardError
