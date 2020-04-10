a9382b1a1793ba618c49f132d3d83b78038875f2
Ray Alva <rayalva407@gmail.com>
2020-04-10 15:00:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,6 @@ class Triangle
   
   def invalid?
     if ((@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)) || ((@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1))
-      binding.pry
   end
     
   def kind
